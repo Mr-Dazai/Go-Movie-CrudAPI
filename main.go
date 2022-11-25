873d302f4b40e9158f06617545b7eb950938f093
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2022-11-24 21:41:47 -0500

    deleteMovies

--- main.go
+++ main.go
@@ -26,6 +26,16 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
 func main() {
 	r := mux.NewRouter()
 	movies = append(movies, Movie{Id: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "Jhone", Lastname: "Doe"}})
