07a42a250531a94ce03c216d40865f2fa2dd65d1
Sebin Joe Paul C.M <sebin.joepaul@gmail.com>
2022-11-24 21:34:30 -0500

    Main.go

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	Id       string    `json:"id"`
+	Isbn     string    `jason:"isbn"`
+	Title    string    `jason:"title"`
+	Director *Director `jason:"director"`
+}
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+func main() {
+	r := mux.NewRouter()
+	movies = append(movies, Movie{Id: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "Jhone", Lastname: "Doe"}})
+	movies = append(movies, Movie{Id: "2", Isbn: "438228", Title: "Movie Two", Director: &Director{Firstname: "sebin", Lastname: "joe"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovies).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovies).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovies).Methods("DELETE")
+
+	fmt.Printf("String server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
